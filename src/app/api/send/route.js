0cedfb55361428c074406b1bb60e55847f349db4
otavio-n <nishimuraotavio@gmail.com>
2025-12-05 11:27:51 -0300

    remove email section

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,15 +2,14 @@ import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+const myEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail, email],
+      from: email,
+      to: [myEmail],
       subject: subject,
       react: (
         <>
